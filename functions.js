e9bb2a3f1a7d1ed73c4a402747e6a6678b151c25
john alejandro <john.e.alejandro01@gmail.com>
2021-08-13 11:24:25 -0500

    Redo

--- functions.js
+++ functions.js
@@ -4,9 +4,7 @@
  * TODO:
  * Create a function called 'sayHello' that takes a parameter 'name'.
  * When called, the function should return a message that says hello to the passed in name.
- * function sayHello ("John") {
- *      consolelog.("Hello")
- *  }
+
  * Example
  * > sayHello("codeup") // returns "Hello, codeup!"
  */
@@ -81,3 +79,6 @@ var random = Math.floor((Math.random() * 3) + 1);
  *
  * > applyDiscount(45.99, 0.12) // 40.4712
  */
+function sayHello ("John") {
+    consolelog.("Hello");
+}
