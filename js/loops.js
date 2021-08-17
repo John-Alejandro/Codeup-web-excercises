598dab8ac96debabb61a8fc23d4735949c035706
john alejandro <john.e.alejandro01@gmail.com>
2021-08-17 11:22:54 -0500

    test2

--- js/loops.js
+++ js/loops.js
@@ -1,7 +1,12 @@
-console.log("Loops is working.");
+console.log("Loops is working.")
 )
 var i =0;
 while( i < 10) {
     console.log("Count to 10");
     i++;
+}
+ var i i = 10:
+while ( i > 10) {
+ i--:
+console.log("Hey there")
 }
