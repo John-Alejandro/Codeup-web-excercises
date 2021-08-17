150e704a28b1a9bf74993a5365064f7bcd45a905
john alejandro <john.e.alejandro01@gmail.com>
2021-08-17 11:39:26 -0500

    new attempt

--- js/loops.js
+++ js/loops.js
@@ -1,5 +1,5 @@
 console.log("Loops is working.")
-)
+
 var i =0;
 while( i < 10) {
     console.log("Count to 10");
@@ -8,5 +8,5 @@ while( i < 10) {
  var i i = 10:
 while ( i > 10) {
  i--:
-console.log("Hey there")
+console.log("Hey there": + i);
 }
