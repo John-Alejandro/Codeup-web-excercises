006339b964b22baa8a165259559290a67bfe7e51
john alejandro <john.e.alejandro01@gmail.com>
2021-08-18 10:06:05 -0500

    First attemt at complete for loop questions.

--- js/for_loops.js
+++ js/for_loops.js
@@ -6,13 +6,28 @@ console.log("Yup, \"fruit\" for_loops.js is up and running.")
 //     }
 // }
 
-function oddRandomNumber() {
-    for ( var i = 20; i <= 200; i++) {
+function oddEvenRandomNumber() {
+    for ( var i = 19; i <200; i++) {
         if ( i % 3 === 0)
         console.log(i + " is odd.");
         else if (i % 2 === 0)
         console.log(i + " is even.");
     }
 }
+// oddEvenRandomNumber()
 
+function numberPyramid() {
+    for ( var i = 1; i <= 9; i++) {
+        console.log(i + i);
 
+    }
+}
+
+// numberPyramid()
+
+function countDown() {
+    for (var i = 100; i >= 5; i-- )
+        console.log (i)
+}
+
+// countDown()
