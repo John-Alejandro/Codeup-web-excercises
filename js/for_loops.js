81136023214f048ff29eea9b7a26eb3179700867
john alejandro <john.e.alejandro01@gmail.com>
2021-08-17 23:18:44 -0500

    Correct showMultiplication without extra code line

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,7 +2,6 @@ console.log("Yup, \"fruit\" for_loops.js is up and running.")
 
 function showMultiplicationTable(x) {
     for ( var i = 1; i <= 10; i++) {
-        for ( var j = 1; j <= 10; j++)
         console.log( x + " x " + i + " = " + i*x)  ;
     }
 }
