9d7a05a1bc00bc65b5bfaaa9b6136838a79a3a4d
john alejandro <john.e.alejandro01@gmail.com>
2021-08-18 16:29:12 -0500

    closing out

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,8 @@
 console.log("Break_and_Continue.js is in the \"House\".")
 
-var x = 0;
 
-var prompt("Pick a number between 1 and 50?");
-while (false) {
-    x++;
-    if ( x % 2 === 0) {
-        alert(" Pick another number.")
-        else ( x % 3 ===)
-    }
-}
+
+    var prompt ("Pick an odd number between 1 and 50?");
+
+
+
