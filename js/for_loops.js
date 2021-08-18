c17d987720bbb3f831f1faf0ccf66a7c5736c6d5
john alejandro <john.e.alejandro01@gmail.com>
2021-08-18 08:45:59 -0500

    oddEven Random Number Code

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,18 @@
 console.log("Yup, \"fruit\" for_loops.js is up and running.")
 
-function showMultiplicationTable(x) {
-    for ( var i = 1; i <= 10; i++) {
-        console.log( x + " x " + i + " = " + i*x)  ;
+// function showMultiplicationTable(x) {
+//     for ( var i = 1; i <= 10; i++) {
+//         console.log( x + " x " + i + " = " + i*x)  ;
+//     }
+// }
+
+function oddRandomNumber() {
+    for ( var i = 20; i <= 200; i++) {
+        if ( i % 3 === 0)
+        console.log(i + " is odd.");
+        else if (i % 2 === 0)
+        console.log(i + " is even.");
     }
 }
+
 
