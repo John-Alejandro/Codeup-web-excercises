6ecd5991b9b01c50371924b003326d89b5f05054
john alejandro <john.e.alejandro01@gmail.com>
2021-08-18 20:49:29 -0500

    completed copying to new file

--- js/external.js
+++ js/external.js
@@ -1,7 +1,10 @@
 alert("Welcome to my website")
 
+
 var userInput = prompt("What is your favorite color?");
     alert("Wow!! " + userInput + ", that's also my favorite color too!");
+
+
 
 var a = prompt("How many days are you renting The Little Mermaid?");
 
@@ -14,3 +17,29 @@ var c = prompt("How many days are you renting Hercules?");
 var num3 = Number(c);
 var result = Number(a)+Number(b)+Number(c);
 alert("Your total is $" + result * 3 );
+
+
+
+var a = prompt("How many hours did you work for Google this week?");
+
+var num1 = Number(a);
+var b = prompt("How many hours did you work for Amazon this week?");
+
+var num2 = Number(b);
+var c = prompt("How many hours did you work for Facebook this week?");
+var num3 = Number(c);
+var result = Number(a * 400)+Number(b * 380)+Number(c * 350);
+alert("Your pay check will be $" + result + " for the hours worked this week");
+console.log("Cost of Rentals.");
+
+
+var userInput = prompt("Is the class full?");
+console.log(userInput);
+var userReply = prompt(" Does enrolling in this class conflict with her schedule?");
+console.log(userReply);
+
+
+var custReply = prompt("If you buy 2 or more items, I can apply the offer? Are you interested in this offer?");
+console.log(custReply);
+var Custreply = prompt("Would you like to become a Premium member? Their are no minimum purchase requirements.");
+console.log(Custreply);
