2a670917b658020a5410275226dac9b8051d74ad
john alejandro <john.e.alejandro01@gmail.com>
2021-08-18 20:33:48 -0500

    copying files over to external.js from js

--- js/external.js
+++ js/external.js
@@ -3,3 +3,14 @@ alert("Welcome to my website")
 var userInput = prompt("What is your favorite color?");
     alert("Wow!! " + userInput + ", that's also my favorite color too!");
 
+var a = prompt("How many days are you renting The Little Mermaid?");
+
+var num1 = Number(a);
+var b = prompt("How many days are you renting Brother Bear?");
+
+var num2 = Number(b);
+var c = prompt("How many days are you renting Hercules?");
+
+var num3 = Number(c);
+var result = Number(a)+Number(b)+Number(c);
+alert("Your total is $" + result * 3 );
