d935c3d24e87d9dcf4295f4dbe8280e72c82d3d6
john alejandro <john.e.alejandro01@gmail.com>
2021-08-25 10:36:41 -0500

    ToDo solutions for first 3 questions

--- js/objects.js
+++ js/objects.js
@@ -126,3 +126,33 @@ console.log()
      */
 
 })();
+// Create a function named rollSixSided that takes in a number as a parameter. Write code within your function that would generate the number between 1 and 6 [the dice] and then console.log the result to the user. Your loop should run the number of times as specified by the argument provided.
+//     Example:
+// rollSixSided(10)
+// Result:
+//     You rolled a 1
+// You rolled a 5
+// You rolled a 2
+// You rolled a 3
+// You rolled a 6
+// You rolled a 6
+// You rolled a 3
+// You rolled a 1
+// You rolled a 6
+// You rolled a 4
+
+
+// function rollSixSided(number) {
+//     return "You rolled a " + number;
+//     var dice = {
+//         roll:1,
+//         roll:2
+//         roll:3,
+//         roll:4,
+//         roll:5,
+//         roll:6
+//     }
+// }
+// console.log(rollSixSided())
+//
+
