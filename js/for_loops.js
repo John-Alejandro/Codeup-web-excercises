f705c858883ae5a6c7d0b5794abd19d535b612a9
john alejandro <john.e.alejandro01@gmail.com>
2021-08-26 16:41:29 -0500

    added questions to solutions

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,7 @@
 console.log("Yup, \"fruit\" for_loops.js is up and running.")
-
+// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+//
+// For example, showMultiplicationTable(7) should output
 function showMultiplicationTable(x) {
     for ( var i = 1; i <= 10; i++) {
         console.log( x + " x " + i + " = " + i*x)  ;
@@ -7,8 +9,11 @@ function showMultiplicationTable(x) {
 }
 showMultiplicationTable(7)
 
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 
-var random = Math.floor((Math.random() * 3) + 1);
+
+
+    var random = Math.floor((Math.random() * 3) + 1);
 var i = random;
 function oddEvenRandomNumber() {
     for ( var i = 19; i <200; i++) {
@@ -22,7 +27,7 @@ function oddEvenRandomNumber() {
 
 
 oddEvenRandomNumber()
-
+// Create a for loop that uses console.log to create the output shown below.
 function numberPyramid() {
     for ( var i = 1; i <= 9; i++) {
         var number = "";
@@ -35,7 +40,7 @@ function numberPyramid() {
 }
 
 numberPyramid()
-
+// Create a for loop that uses console.log to create the output shown below.
 function countDown() {
     for (var i = 100; i >= 5; i=i-5 )
         console.log (i)
