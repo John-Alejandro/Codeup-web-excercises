5ba2944edfb8eb9736f3e8059edfa0280bcf0294
john alejandro <john.e.alejandro01@gmail.com>
2021-08-28 18:11:17 -0500

    possible solution

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,19 +1,17 @@
 console.log("Break_and_Continue.js is in the \"House\".")
 
 // Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
-//     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 
 
-
-
-
-    for (var i = 1; i <= 50; i++) {
-        if (i % 2 !== 0) {
-            continue;
-        }
-        var PickoddNumber  = prompt("Pick an odd number between 1 and 50?");
+function PickoddNumber() {
+    while(true) {
+var PickoddNumber  = prompt("Pick an odd number between 1 and 50?");
+    if ( PickoddNumber % 2 !== 1 && PickoddNumber <=  50 && PickoddNumber >= 1)
         alert("Please try again, you did not pick an odd number.");
-    }
+            break;
+
+        }
+}
 
 
-
+//     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
