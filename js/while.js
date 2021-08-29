68a832b1c9f33f7fee6f9e4e3063e3a5b1e64575
john alejandro <john.e.alejandro01@gmail.com>
2021-08-28 19:43:51 -0500

    completed reworked solution

--- js/while.js
+++ js/while.js
@@ -1,1 +1,30 @@
-console.log("While you were waiting.")
+
+// Create a while loop that uses console.log() to create the output shown below.
+//
+//
+// 2
+// 4
+// 8
+// 16
+// 32
+// 64
+// 128
+// 256
+// 512
+// 1024
+// 2048
+// 4096
+// 8192
+// 16384
+// 32768
+// 65536
+
+function MultiplybyTwo () {
+    var number = 1;
+
+while (number < 65536)  {
+    number *= 2;
+    console.log(number);
+    }
+
+}
