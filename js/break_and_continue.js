ff41364d19e933e4e3bf0d9a2e357113c18f8a99
john alejandro <john.e.alejandro01@gmail.com>
2021-08-28 19:05:35 -0500

    reworked solutions after viewing review

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,27 +1,29 @@
 console.log("Break_and_Continue.js is in the \"House\".")
 
 // Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
-
-
-function PickoddNumber() {
-    while(true) {
-var PickoddNumber  = prompt("Pick an odd number between 1 and 50?");
-    if ( PickoddNumber % 2 !== 1 && PickoddNumber <=  50 && PickoddNumber >= 1)
-        alert("Please try again, you did not pick an odd number.");
-            break;
-
-        }
-}
-
-
 //     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 
 
-for (var i =1; i<= 5; i +=1) {
-    if (i === PickoddNumber) {
-        var PickoddNumber = prompt("Whatever odd number you pick between 1 and 50. I will skip it.")
-        console.log("Skipping your number, " + PickoddNUmber);
-        continue;
 
+function PickoddNumber() {
+    var userInput;
+
+    while (true) {
+        userInput = prompt("Pick an odd number between 1 and 50?");
+
+        if (userInput % 2 !== 0 && userInput > 0 && userInput < 51) {
+            break;
+        }
+    }
+
+    console.log("Number to skip is: " + userInput);
+
+    for (var i = 1; i < 50; i += 2) {
+        if (i == userInput) {
+            var PickoddNumber = prompt("Whatever odd number you pick between 1 and 50. I will skip it.")
+            console.log("Skipping your number, " + i);
+            continue;
+
+        }
     }
 }
