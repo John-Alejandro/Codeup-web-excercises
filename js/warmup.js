5bbdeea69658623f558acf1331320034e81ce5c4
john alejandro <john.e.alejandro01@gmail.com>
2021-09-03 14:01:24 -0500

    Completed several taks upto fieldset

---
+++ js/warmup.js
@@ -0,0 +1,15 @@
+function tellfortune(c,s,j,C) {
+    var c = Math.random(c);
+    var s = ;
+    var j = random.job
+    var C = random.city
+    console.log( "You have "+ c + "children are " + s + " and work as a " + j +  " in " + C ".");
+}
+    //Did not need a return can just console.log!!1
+    console.log(tellfortune())
+
+
+function calculateDogAge(input,conversionNum) {
+    var dogAge = input * conversionNum;
+    return " Your dog is " + input + "years old in dog years. Types " + conversionNum ;
+}
