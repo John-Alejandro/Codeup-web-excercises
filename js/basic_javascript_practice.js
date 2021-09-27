0d70d9d00fbf7fcd64afb4008f1264ab3f1c76da
john alejandro <john.e.alejandro01@gmail.com>
2021-09-26 20:05:44 -0500

    second part of Javascript Basics

--- js/basic_javascript_practice.js
+++ js/basic_javascript_practice.js
@@ -290,4 +290,205 @@
 
 // array.map ////////////////////////////////////////////////////
 console.log("\u{1F600}")
+// let array = [1,3,5,6]
+
+// console.log(array.map((element) => {
+//     return element/3;
+// }))
+
+//  array.filter  //////////////////////////////////////////////
+
+// let array = [1,3,5,6]
+
+// let newArray = array.filter(element => {
+//     if(element > 2) return true;
+//         return false;
+// });
+
+// console.log(newArray);
+
+//   array.forEach  ////////////////////////////////////////////
+
+// let array = ["John", "Fela","Liz", "Joanne"];
+
+// for(let i=0;array.length;i++){
+//     const element = array[i];
+// }
+// These two functions give same result
+// array.forEach((element) => {
+//     console.log(element);
+// });
+
+// array.reduce  ///////////////////////////////////////////////
+
+// let array = [1,2,3,4,5,6,,7,8,9,10]; // -> 55
+
+//  const res = array.reduce((accumulator,currentValue) => {
+//     return accumulator + currentValue;
+// }, 0);
+
+//  accumulator = 0 | currentValue = 1
+//  accumulator = 1| currentValue = 2
+//  accumulator = 3 | currentValue = 3
+//  .....
+//  .. 45
+
+// console.log(res);
+
+//  Array.indexOf,Array.includes,ARRay.find  /////////////
+
+// let myFavFoods = ["pizza", "burgers", "pasta"];
+
+// console.log(myFavFoods.indexOf("pizza"));
+// console.log(myFavFoods.includes("pizza"));
+
+// console.log(myFavFoods.includes("pizza"));  // Turns into a boolean
+
+// let students = [
+//     {
+//         name: "ABC",
+//         ae:15
+//     },
+//     {
+//         name: "DEF",
+//         age: 16
+//     }
+// ]
+
+// const student = students.find(s => {     // Using .find assists in finding object value
+//     return s.name == 15
+// })
+
+// console.log(student)
+
+// == vs. ===  ////////////////////////////////////////////////////////////////////
+
+// let a = //
+// let b = //
+
+// console.log(a === b)
+
+//  1. The types of a and b are same -> value, memory
+//  2. The types pf a and b are different -> type conversion
+
+// let a = 2
+// let b = "2"
+
+// console.log(a === b);
+
+// 1. Check types
+// 2. If type is not the same, and operator is === -> return false
+// 2. If type is not the same, and operator is == -> change the type of 1 variable
+// 3. Compare the values ( types are now the same)
+
+// let b = 0
+
+// if (b !== undefined || b !== false) {     // This testing not defined condition
+//     console.log("b is defined")
+// } else {
+//     console.log("b is not defined")
+// }
+
+//  JS DOM  //////////////////////////////////////////////////////////////////////
+
+// DOM is a tree of elements in JS
+
+//                       Document
+//                 Root Element (<html>)
+//     Element (<head>)              Element(<body>)
+//     Element (<title>)     Element(<h1>)   Element(<p>)
+//      Text "My text"      Text "My Header"  Text My Paragraph"
+
+// Document / Window Object  ////////////////////////////////////////////////////
+
+// document gives you axxess to whole document
+// To view document properties console.dir(document)
+
+// window object has alot of properties- Javascript objects, functions, and variables are members of window object
+
+// window.document == document
+// true
+
+// getElementById //////////////////////////////////////////////////////////////
+
+// document.getElementById("name")
+//     <h1 id="name">My name is John</h1>
+// console.dir(document.getElementById("name"))  // Can view properties of #name, can make changes
+// p #name
+// document.getElementById("name").innerText = 'My name is codedamn"   // Changes are made on document immediately
+
+// Ways to transverse DOM /////////////////////////////////////////////////////
+
+// let selectors = document.getElementsByClassName("name")
+// gives list of :name" class
+// selectors[0].innerText = "First element"    // makes changes
+// selectors[1].innerText = "Second element"   // makes changes
+
+// let elements = document.getElementsByTagName("p")
+// elements // calling gives you list of p
+// elements[0].innerText = "asdffds"  // changes text immediately
+// innerHTML is used when you use <p> </p> format
+
+// setTimeout and setInterval  /////////////////////////////////////////////////////////////////
+
+// console.log("Program starts");
+// let i = 0;
+
+// setInterval(() => {
+//     document.getElementById("name").innerText = ++i;
+// }, 2000)
+
+// console.log("Program ends");
+
+// setTimeout only does one time
+
+// setInterval repeats
+
+// clearTimeout and clearInterval  ///////////////////////////////////////////////////////
+
+// console.log("Program starts");
+// let i = 0;
+
+// let id = setInterval(() => {                            // setInterval stops at set time   // setTimeout stops immediately
+//     document.getElementById("name").innerText = ++i;
+
+//     if (i == 100) {
+//         clearInterval(id);
+
+//     }
+// }, 8000)
+
+// console.log(id);
+
+// console.log("Program ends");
+
+//  Event Handlers  ///////////////////////////////////////////////////////////////////
+
+// const elem = document.getElementById("name");
+
+// elem.onclick = function() {                         // ondbclick  - double click
+//     console.log("Clicked!!");
+// }
+
+// function myFunction() {
+//     console.log("Something else clicked!")
+// }
+
+// Exception/Error Handling ///////////////////////////////////////////////////////////////
+
+// let i = 100;
+// let j = 2;
+
+// try {
+//     if(j == 0){
+//         throw " j cannot be zero"
+//     }
+//     console.log(i/j);
+// } catch (err) {
+//     console.log(err)
+// }
+
+// console.log("Nice little statement");
+
+
 
