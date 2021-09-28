75720fe538dd96f141bdb48d909aae8f275ee54a
john alejandro <john.e.alejandro01@gmail.com>
2021-09-28 14:50:22 -0500

    Will complete first exercise after hours, moving fast and have issue with css showing on page

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,13 +2,14 @@
 // $(document).ready(function () {
 //     alert("The page has loaded!");
 // });
-
-$("sub-title1").ready(function () {
-    alert("This message will self destruct in 10 seconds.")
-});
-
-$("sub-title2").ready(function () {
-    alert("If you choose to accept this mission, Good Luck.")     // message still displays with same id
-})
-
-$("p").css("border","1px","color","red");
+//
+// var yourMission = $("Missions").html(
+// alert(yourMission);
+// );
+//
+// $("sub-title2").ready(function () {
+//     alert("If you choose to accept this mission, Good Luck.")     // message still displays with same id
+// })
+//
+// $(".codeup").css("border","1px solid red");
+// need to rework and complete this first exercises
