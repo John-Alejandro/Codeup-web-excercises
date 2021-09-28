e32ebf6097e81420ce6a3cba2137580886bc47d2
john alejandro <john.e.alejandro01@gmail.com>
2021-09-28 15:55:40 -0500

    Completed several more tasks, still have a few left

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,35 +1,40 @@
 "use strict"
-// $(document).ready(function () {
-//     alert("The page has loaded!");
-// });
-//
-// var yourMission = $("Missions").html(
-// alert(yourMission);
-// );
-//
-// $("sub-title2").ready(function () {
-//     alert("If you choose to accept this mission, Good Luck.")     // message still displays with same id
-// })
-//
-// $(".codeup").css("border","1px solid red");
+$(document).ready(function () {
+    alert("Your page has loaded.");
+});
+
+$("main-heading").ready(function () {
+    var yourMission = $("#main-heading").html();
+    alert(yourMission);
+});
+
+    $("sub-title2").ready(function () {
+    alert("If you choose to accept this mission, Good Luck.")
+});
+
+$(".codeup").css("border","1px solid red");
+
+$("li").css("font-size","20px");
+
+
 // need to rework and complete this first exercises
 
-$("#main-heading").click(function () {
-   // alert("Mission accepted by you clicking");
-   $(this).css("background-color", "red");
-})
-
-$("p").dblclick(function () {
-    $("p").css("font-size", `18px`);
-})
-
-$("li").hover(
-    function () {
-        $(this).css("color", "red");
-    },
-    function () {
-        $(this).css("color", "black");
-    }
-);
+// $("#main-heading").click(function () {
+//    // alert("Mission accepted by you clicking");
+//    $(this).css("background-color", "red");
+// })
+//
+// $("p").dblclick(function () {
+//     $("p").css("font-size", `18px`);
+// })
+//
+// $("li").hover(
+//     function () {
+//         $(this).css("color", "red");
+//     },
+//     function () {
+//         $(this).css("color", "black");
+//     }
+// );
 
 
