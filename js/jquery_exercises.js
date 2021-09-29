2799a3a6fdd9730690e1c23925b546756253ecd6
john alejandro <john.e.alejandro01@gmail.com>
2021-09-28 23:56:47 -0500

    Practicing with mouse event

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,28 +1,50 @@
 "use strict"
-$(document).ready(function () {
-    alert("Your page has loaded.");
-});
+// $(document).ready(function () {
+//     alert("Your page has loaded.");
+// });
+//
+// $("main-heading").ready(function () {
+//     var yourMission = $("#main-heading").html();
+//     alert(yourMission);
+// });
+//
+//     $("sub-title2").ready(function () {
+//     alert("If you choose to accept this mission, Good Luck.")
+// });
 
-$("main-heading").ready(function () {
-    var yourMission = $("#main-heading").html();
-    alert(yourMission);
-});
+// $(".codeup").css("border","1px solid red");
 
-    $("sub-title2").ready(function () {
-    alert("If you choose to accept this mission, Good Luck.")
-});
+$("*").css("background-color","#cccccc")
 
-$(".codeup").css("border","1px solid red");
+$(".codeup").css("border", "25px dashed red","padding","5em");
+
+$(".codeup").css("padding","5em");
+
 
 $("li").css("font-size","20px");
 
+$("li").css("list-style-type","none");
+
+$("li").css("background-color","coral");
+
+
+$("p").css("background-color", "yellow");
+
+$("h1").css("font-size", "80px");
+
+$("h1").css("text-align","center");
+
+$("h1").ready(function () {
+    var MI6 = $("h1").html();
+    alert(MI6);
+})
 
 // need to rework and complete this first exercises
 
-// $("#main-heading").click(function () {
-//    // alert("Mission accepted by you clicking");
-//    $(this).css("background-color", "red");
-// })
+$("#main-heading").click(function () {
+   // alert("Mission accepted by you clicking");
+   $(this).css("background-color", "red");
+})
 //
 // $("p").dblclick(function () {
 //     $("p").css("font-size", `18px`);
