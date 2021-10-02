464ce79b6c2b49b379d0855392a63fa9dc8d4506
john alejandro <john.e.alejandro01@gmail.com>
2021-10-02 14:47:21 -0500

    completed regular tasks, no bonus yet

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -23,25 +23,20 @@ $("#clickForAnswers").click(function (e) {
     $("dd").toggleClass("invisible");
 });
 
-//     Bonus
+
 //
-// When any of the dt elements is clicked, the element that was clicked should be highlighted.
-
-// $("dt").click(function () {
-//     $(this).css({"background-color": "yellow", "width": "11%"})
-// });
-
+//      When any of the dt elements is clicked, the element that was clicked should be highlighted.
 
 //     1. Open the file named jquery_faq.html for editing. Commit all changes to GitHub.
 //      Completed
 //     2. Under the FAQ, add 3 unordered lists like above. Each list should contain a national park name in an h3 element, and a ul of 4 facts about the park.
 //      Completed
 //     3. Create a button that, when clicked, makes the last li in each ul have a yellow background.
-//
+//      Completed
 //     4. When any h3 is clicked, the lis underneath it should be bolded. Use font-weight: bold to achieve this.
-//
+//      Completed
 //     5. When any list item is clicked, first li of the parent ul should have a font color of blue.
-//
+//      Completed
 //     Bonus
 //     Create 3 divs that each look like a picture frame. Each one should have a unique background image and a button underneath that swaps the image from the frame. Use the traversing functions of jQuery to accomplish this.
 //
@@ -51,12 +46,6 @@ $("#clickForAnswers").click(function (e) {
 //     The center frame swaps randomly to either the left or right.
 //     The right frame swaps to the left and takes the image from the frame in the center.
 
-// $("#makeYellow").click(function () {
-//     e.preventDefault();
-//     $(this).each(function () {
-//         $(this).children().last().css("background-color","yellow");
-//     })
-// })
 
 
 
