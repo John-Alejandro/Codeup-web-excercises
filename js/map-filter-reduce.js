400f7681cde7c9d460efce9a70d43617039faba1
john alejandro <john.e.alejandro01@gmail.com>
2021-10-14 12:00:58 -0500

    completed .map and 1st .reduce tasks

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -43,19 +43,31 @@ let usersWithMutipleLangs = users.filter(function (language) {
     return language.languages.length > 2;
 })
 
+console.log(usersWithMutipleLangs);
+
 //     Use .map to create an array of strings where each element is a user's email address
 
+let userEmails = users.map(function (useremail) {
+    return useremail.email;
+})
 
+console.log(userEmails);
 
 // Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 
+let totalUserExperience = users.reduce(function (result,user) {
+    return result + user.yearsOfExperience/users.length;
+}, 0)
 
-
+console.log(totalUserExperience);
 //     Use .reduce to get the longest email from the list of users.
 
+let longestUserEmail = users.reduce(function (useremail) {
+    return useremail.email;
+})
 
+console.log(Math.max(longestUserEmail));
 //     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
 
 
-console.log(usersWithMutipleLangs);
