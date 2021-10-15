12f834bc2818f34ff90313cd6f13afff9ec89bef
john alejandro <john.e.alejandro01@gmail.com>
2021-10-15 10:41:11 -0500

    Got token from Github

--- js/promises.js
+++ js/promises.js
@@ -5,7 +5,7 @@ console.log("Promise");
 // Create a file named promises.js inside of your js directory and link it to an HTML file of your choice.
 // Completed file setup
 //     Generate a Personal Access Token for the GitHub API.
-//
+// Completed
 //     We will use this so that we don't get rate limited when talking to the GitHub API. You can add the token to your requests like this:
 //
 //
