f0f550c0fccc1c0ce2e4ae49bc1b5b59d16f2016
john alejandro <john.e.alejandro01@gmail.com>
2021-10-16 23:48:41 -0500

    Trying to get response to work,still getting forEach error

--- js/promises.js
+++ js/promises.js
@@ -9,20 +9,32 @@
 // Completed
 //     We will use this so that we don't get rate limited when talking to the GitHub API. You can add the token to your requests like this:
 //
-var url = "https://api.github.com/users/John-Alejandro/events";
-// console.log(url);
+var url = "https://api.github.com/users/John-Alejandro/events/public";
+console.log(url);
 
-fetch(url, {headers: {'Authorization': promiseAPIKey}});
+fetch("https://api.github.com/users/John-Alejandro/events/public", {headers: {'Authorization': promiseAPIKey}});
 
 // Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
 
-var myGitHubApi = fetch("https://api.github.com/users/John-Alejandro/events")
+fetch('https://api.github.com/users/John-Alejandro/events/public')
 
     .then((response) => {
+        // console.log(response.json());
         return response.json();
-    }).then((myGitHubAPI)=>{
-        console.log(myGitHubApi.results);
-        myGitHubApi.results.forEach((event)=> {
-            console.log(event.created_at)
+    }).then((myGitHubEventsData)=>{
+        console.log(myGitHubEventsData.results);
+        myGitHubEventsData.results.forEach((event)=> {
+            console.log(event.id)
         })
     })
+
+fetch('https://swapi.dev/api/films')
+
+    .then((response)=>{
+        // console.log(response.json());
+        return response.json();
+    }).then((starWarsFilmsData)=>{
+    //starWarsFilmsData is the parsed object from the call to response.json()
+    console.log(starWarsFilmsData.results);
+    starWarsFilmsData.results.forEach((film)=>{console.log(film.title)})
+})
